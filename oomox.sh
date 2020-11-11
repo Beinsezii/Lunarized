620628e48086da0fb82bb5aa7202dc1bfad935e7
Beinsezii <beinsezii@gmail.com>
2020-11-11 13:45:00 -0800

    Updated with new flipped oomox themes

--- oomox.sh
+++ oomox.sh
@@ -2,13 +2,17 @@
 # thx stackoverflow
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR
-rm -rf ./Lunarized/GTK
+rm -r ./Lunarized/GTK
 mkdir ./Lunarized/GTK
 
 cd ~/.themes/
 zip -ryq9 theme.zip oomox-Lunarized
 mv theme.zip "${DIR}/Lunarized/GTK"
+zip -ryq9 theme_flipped.zip oomox-Lunarized_Flipped
+mv theme_flipped.zip "${DIR}/Lunarized/GTK"
 cd ~/.icons/
 zip -ryq9 icons.zip oomox-Lunarized
 mv icons.zip "${DIR}/Lunarized/GTK"
+zip -ryq9 icons_flipped.zip oomox-Lunarized_Flipped
+mv icons_flipped.zip "${DIR}/Lunarized/GTK"
 cd $DIR
